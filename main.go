4338ee910b2fe01249558556f12f509ba9d7d8a9
liulinlin <liulinlin@zhumengyuan.com>
2016-01-07 17:20:10 +0800

    update code

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/hu17889/go_spider/core/common/page"
-	"github.com/hu17889/go_spider/core/common/request"
+	"github.com/hu17889/go_spider/core/pipeline"
+	"github.com/hu17889/go_spider/core/scheduler"
 	"github.com/hu17889/go_spider/core/spider"
 	"regexp"
 	"strings"
@@ -19,8 +20,6 @@ func NewMyPageProcesser() *MyPageProcesser {
 
 var baikeUrlReg = regexp.MustCompile(`^http://baike\.baidu\.com/view/.*?`)
 
-// Parse html dom here and record the parse result that we want to crawl.
-// Package goquery (http://godoc.org/github.com/PuerkitoBio/goquery) is used to parse html.
 func (this *MyPageProcesser) Process(p *page.Page) {
 	if !p.IsSucc() {
 		println(p.Errormsg())
@@ -56,26 +55,9 @@ func (this *MyPageProcesser) Finish() {
 }
 
 func main() {
-	// spider input:
-	//  PageProcesser ;
-	//  task name used in Pipeline for record;
-	sp := spider.NewSpider(NewMyPageProcesser(), "TaskName")
-	// GetWithParams Params:
-	//  1. Url.
-	//  2. Responce type is "html" or "json" or "jsonp" or "text".
-	//  3. The urltag is name for marking url and distinguish different urls in PageProcesser and Pipeline.
-	//  4. The method is POST or GET.
-	//  5. The postdata is body string sent to sever.
-	//  6. The header is header for http request.
-	//  7. Cookies
-	req := request.NewRequest("http://baike.baidu.com/view/39352.htm", "html", "", "GET", "", nil, nil, nil, nil)
-	pageItems := sp.GetByRequest(req)
-	//pageItems := sp.Get("http://baike.baidu.com/view/1628025.htm?fromtitle=http&fromid=243074&type=syn", "html")
-
-	url := pageItems.GetRequest().GetUrl()
-	println("-----------------------------------spider.Get---------------------------------")
-	println("url\t:\t" + url)
-	for name, value := range pageItems.GetAll() {
-		println(name + "\t:\t" + value)
-	}
+	sp := spider.NewSpider(NewMyPageProcesser(), "TaskName").
+		SetScheduler(scheduler.NewQueueScheduler(true)).
+		AddPipeline(pipeline.NewPipelineFile("result.txt"))
+	sp.AddUrl("http://baike.baidu.com/view/39352.htm", "html")
+	sp.Run()
 }
